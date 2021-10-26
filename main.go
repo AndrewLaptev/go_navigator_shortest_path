d2e530580a3b1ff5b2fba02ccede058bb691dcd4
Andrew Laptev <nickname.avast@gmail.com>
2021-10-26 03:46:34 +0300

    Add functions createGraphNumFile(), createMapUndirGraph(), createMapDirGraph(), createMapsRelVertsArcs(), pathToSymb(), vertToNum(). Add input names of source and destination cabinets in main().

--- main.go
+++ main.go
@@ -13,19 +13,30 @@ import (
 const (
 	nameSymbFile = "graph_symbolic.txt"
 	nameNumFile  = "graph_numeric.txt"
+	// src          = "A274"
+	// dst          = "A258"
 )
 
 func main() {
-	fileSymb, err := os.Open(nameSymbFile)
-	if err != nil {
-		panic(err)
-	}
-	defer fileSymb.Close()
+	mapVerts := createGraphNumFile(nameSymbFile, nameNumFile)
 
-	// fmt.Println("Shortest distance ", best.Distance, " following path ", best.Path)
-	fmt.Println(createMapVertsSymbNum(fileSymb))
+	var src, dst, ok string
+	for {
+		fmt.Println("Please enter source cabinet:")
+		fmt.Scanln(&src)
+		fmt.Println("Please enter destination cabinet:")
+		fmt.Scanln(&dst)
+		distance, path := shortestPath(nameNumFile, vertToNum(src, mapVerts), vertToNum(dst, mapVerts))
+		fmt.Printf("Shortest distance between %s and %s is %d following path %v\n", src, dst, distance, pathToSymb(path, mapVerts))
+		fmt.Println("Are you want to exit?:")
+		fmt.Scanln(&ok)
+		if ok == "yes" {
+			break
+		}
+	}
 }
 
+// Find shortest path between two vertexs of graph from numeric file
 func shortestPath(nameGraphFile string, src, dst int) (int64, []int) {
 	graph, _ := dijkstra.Import(nameGraphFile)
 	best, err := graph.Shortest(src, dst)
@@ -35,40 +46,108 @@ func shortestPath(nameGraphFile string, src, dst int) (int64, []int) {
 	return best.Distance, best.Path
 }
 
-// Create map of graph vertexs from symbolic file to numeric list
-// должна вернуть все таки две мапы: мапа вершин назв-номер и мапу ребер вершин номер верш-строковый массив ребер
-func createMapVertsSymbNum(symbFileName *os.File) map[string]int {
-	var mapVertsSymbNum = make(map[string]int)
+// Translate vertex symbolic name to numeric
+func vertToNum(name string, relation map[string]int) int {
+	return relation[name]
+}
+
+// Translate numeric view of path to symbolic view
+func pathToSymb(path []int, relation map[string]int) []string {
+	var strPath = make([]string, 0)
+	for _, v := range path {
+		for i1, v1 := range relation {
+			if v1 == v {
+				strPath = append(strPath, i1)
+			}
+		}
+	}
+	return strPath
+}
+
+// Create two maps relation of graph vertexs and arcs from symbolic file
+func createMapsRelVertsArcs(symbFileName *os.File) (map[string]int, map[int][]string) {
+	var mapVerts = make(map[string]int)
+	var mapVertsArcs = make(map[int][]string)
 
 	scanner := bufio.NewScanner(symbFileName)
 	for i := 0; scanner.Scan(); i++ {
-		vertSymb := strings.Split(scanner.Text(), " ")[0]
-		mapVertsSymbNum[vertSymb] += i
+		vertStr := strings.Split(scanner.Text(), " ")[0]
+		mapVerts[vertStr] += i
+		arcsStrArr := strings.Split(scanner.Text(), " ")[1:]
+		mapVertsArcs[mapVerts[vertStr]] = arcsStrArr
 	}
-
-	return mapVertsSymbNum
+	return mapVerts, mapVertsArcs
 }
 
+// Create weighted map of direction graph from two relation maps
 func createMapDirGraph(mapVerts map[string]int, mapVertsArcs map[int][]string) map[int][]int {
+	var mapDirGraph = make(map[int][]int)
 
+	for i, v := range mapVertsArcs {
+		mapDirGraph[i] = func() []int {
+			var numArr = make([]int, 0)
+			for _, v1 := range v {
+				strArr := strings.Split(v1, ",")
+				numArr = append(numArr, mapVerts[strArr[0]])
+				numArr = append(numArr, int(([]rune(strArr[1])[0] - '0')))
+			}
+			return numArr
+		}()
+	}
+	return mapDirGraph
 }
 
-// Create numeric map vertexs and arcs of graph from symbolic file.
-// For undir graph arcs are duplicated
-func createMapNumUndirGraph(symbFileName *os.File, mapVertsSymbNum map[string]int) map[int]int {
-	var mapNumUndirGraph = make(map[int]int)
-	return mapNumUndirGraph
+// Create map of undirection graph from direction graph map
+func createMapUndirGraph(mapDirGraph map[int][]int) map[int][]int {
+	var mapUndirGraph = mapDirGraph
+
+	for i, v := range mapDirGraph {
+		for i1 := 0; i1 < len(v); i1 += 2 {
+			check := func() bool {
+				for i2 := 0; i2 < len(mapUndirGraph[v[i1]]); i2 += 2 {
+					if mapUndirGraph[v[i1]][i2] == i {
+						return true
+					}
+				}
+				return false
+			}()
+			if check {
+				continue
+			} else {
+				mapUndirGraph[v[i1]] = append(mapDirGraph[v[i1]], i, v[i1+1])
+			}
+		}
+	}
+	return mapUndirGraph
 }
 
-func createUndirGraphNumFile(mapVertsSymbNum map[string]int, fileName string) {
-	file, err := os.Create(fileName)
+// Create numeric file of undirect graph from symbolic graph file (direct).
+//  Return map of relation symbolic name of vertexs and numeric name of vertexs
+func createGraphNumFile(symbFileName, numFileName string) map[string]int {
+	fileSymb, err := os.Open(nameSymbFile)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	mapVertsArcs := make(map[int][]string)
-	for i, v := range mapVertsSymbNum {
-		mapVertsArcs[v] =
+	defer fileSymb.Close()
+	fileNum, err := os.Create(numFileName)
+	if err != nil {
+		panic(err)
 	}
+	defer fileNum.Close()
+
+	mapVerts, mapVertsArcs := createMapsRelVertsArcs(fileSymb)
+	mapUndirGraph := createMapUndirGraph(createMapDirGraph(mapVerts, mapVertsArcs))
+
+	for i, v := range mapUndirGraph {
+		str := fmt.Sprintf("%d ", i)
+		for i1, v1 := range v {
+			if i1%2 != 0 {
+				str += fmt.Sprintf(",%d ", v1)
+				continue
+			}
+			str += fmt.Sprintf("%d", v1)
+		}
+		fileNum.WriteString(str + "\n")
+	}
+	return mapVerts
 }
